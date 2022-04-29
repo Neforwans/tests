4b7349cfd2570ee2d2e3d680cb0827cbd052308d
Neforwans <neforwan11@gmail.com>
2022-04-29 18:41:22 +0200

    Initial commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog_list")
+     */
+
+    public function list()
+    {
+       echo 'hello world';
+    }
+}
